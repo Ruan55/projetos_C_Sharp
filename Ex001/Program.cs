0bc0a2e3ca529e69aa00560e54ab57961ee1c4a5
Ruan55 <114105714+Ruan55@users.noreply.github.com>
2025-06-21 12:36:50 -0300

    Add files via upload
    Exercicio 001

---
+++ Ex001/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex001
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string nome = "Ruan";
+            int idade = 22;
+            float altura = 1.80f;
+            bool souEstudante = true;
+
+            Console.WriteLine($"\nNome: {nome}\nIdade: {idade}\nAltura: {altura}\nSou Estudante: {souEstudante}\nDobro da idade: {idade * 2}");
+
+        }
+    }
+}
