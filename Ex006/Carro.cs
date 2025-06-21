d3644c0778d5e1d728e331e16fe5c22c77c0f6bd
Ruan55 <114105714+Ruan55@users.noreply.github.com>
2025-06-21 12:41:11 -0300

    Add files via upload

---
+++ Ex006/Carro.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex006
+{
+    public class Carro
+    {
+
+        public string marca;
+        public string modelo;
+        public int ano;
+
+        public void ExibirInformacoes()
+        {
+
+            Console.WriteLine($"Marca: {marca}\nModelo: {modelo}\nAno: {ano}");
+
+        }
+
+    }
+}
---
+++ Ex006/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex006
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Carro carro1 = new Carro();
+            Carro carro2 = new Carro();
+
+            carro1.marca = "Toyota";
+            carro1.modelo = "Corolla";
+            carro1.ano = 2020;
+
+            carro2.marca = "Honda";
+            carro2.modelo = "Civic";
+            carro2.ano = 2018;
+
+            Console.WriteLine("Carro 1: ");
+            carro1.ExibirInformacoes();
+
+            Console.WriteLine("\n");
+
+            Console.WriteLine("Carro 2: ");
+            carro2.ExibirInformacoes();
+
+        }
+    }
+}
