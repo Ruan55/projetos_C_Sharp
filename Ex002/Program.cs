48062b86acc358a6097545bc85d49a9f83f73f9d
Ruan55 <114105714+Ruan55@users.noreply.github.com>
2025-06-21 12:37:17 -0300

    Add files via upload
    Exercicio 002

---
+++ Ex002/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex002
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Digite a sua idade: ");
+            int idadeUsuario = int.Parse(Console.ReadLine());
+
+            if(idadeUsuario < 18)
+            {
+
+                Console.WriteLine("Menor de Idade");
+
+            }
+
+            else if(idadeUsuario >= 18 && idadeUsuario <= 59)
+            {
+
+                Console.WriteLine("Adulto");
+
+            }
+
+            else
+            {
+
+                Console.WriteLine("Idoso");
+
+            }
+
+            Console.WriteLine("\n");
+
+            Console.WriteLine("Escolha um número de 1 a 7");
+            int diaDaSemana = int.Parse(Console.ReadLine());
+
+            switch (diaDaSemana)
+            {
+
+                case 1:
+                    Console.WriteLine("Domingo");
+                    break;
+
+                case 2:
+                    Console.WriteLine("Segunda-feira");
+                    break;
+
+                case 3:
+                    Console.WriteLine("Terça-feira");
+                    break;
+
+                case 4:
+                    Console.WriteLine("Quarta-feira");
+                    break;
+
+                case 5:
+                    Console.WriteLine("Quinta-feira");
+                    break;
+
+                case 6:
+                    Console.WriteLine("Sexta-feira");
+                    break;
+
+                case 7:
+                    Console.WriteLine("Sábado");
+                    break;
+
+                default:
+                    Console.WriteLine("Dia inválido");
+                    break;
+
+            }
+
+        }
+    }
+}
