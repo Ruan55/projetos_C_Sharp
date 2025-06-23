06063aea20a4e360db8be2a05ed8ee4103dc79be
Ruan55 <114105714+Ruan55@users.noreply.github.com>
2025-06-23 12:05:09 -0300

    Add files via upload
    Projeto 001

---
+++ projeto_pratico_001/Cliente.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projeto_pratico_001
+{
+    internal class Cliente
+    {
+
+        public string nome;
+        public int idade;
+        public string email;
+
+        public void ExibirDados()
+        {
+
+            Console.WriteLine($"Nome: {nome} \nIdade: {idade} \nEmail: {email}");
+
+        }
+
+    }
+}
---
+++ projeto_pratico_001/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projeto_pratico_001
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<Cliente> clientes = new List<Cliente>();
+
+            for(int i = 1; i <= 3; i++)
+            {
+
+                Cliente cliente = new Cliente();
+                Console.WriteLine($"Digite o nome do cliente {i}: ");
+                cliente.nome = Console.ReadLine();
+
+                Console.WriteLine("Digite a idade: ");
+                cliente.idade = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Digite o email: ");
+                cliente.email = Console.ReadLine();
+
+                Console.WriteLine("\n");
+
+                clientes.Add(cliente);
+
+            }
+
+            Console.WriteLine("Lista de Clientes Cadastrados: ");
+            foreach(var cliente in clientes)
+            {
+
+                cliente.ExibirDados();
+                Console.WriteLine("\n");
+
+            }
+
+        }
+    }
+}
